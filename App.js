9535d8206d4a95a3f788e399fe016763670602fe
unknown <you@example.com>
2026-05-19 18:02:42 -0300

    app filmes adição de imagen e estilos

--- App.js
+++ App.js
@@ -31,16 +31,23 @@ export default function App() {
      <View style = {styles.containerSearch}>
       <TextInput 
       placeholder= 'Buscar filme... '
+      style = {styles.inputSearch}
       ></TextInput>
 
+      <TouchableOpacity>
+      <Feather name="search" size={24} color="black" />
+      </TouchableOpacity>
      </View>
-
-
 
 
       {/* INICIO DO BANNER */}
       
-      
+      <Text style ={styles.textBanner}> Em cartaz </Text>
+
+  <Image style = {styles.imageBanner}
+  source = {require("./assets/smallbanner.avif")}
+ 
+  />
       
       
     </View>
@@ -60,7 +67,14 @@ const styles = StyleSheet.create({
   width: "90%",
   alignItems: 'center',
   marginTop: 10,
-justifyContent: 'space-between'
+  justifyContent: 'space-between'
+},
+
+  inputSearch:{
+  height: 40,
+  padding: 8,
+  width: '100%',
+  alignItems: "center"
 },
 
   textHeader:{
@@ -74,9 +88,27 @@ containerSearch:{
   marginTop: 20,
   width: "90%",
   backgroundColor: 'white',
-  borderRadius: 2
+  borderRadius: 2,
+  flexDirection: 'row',
+  justifyContent: 'space-between'
+},
+
+imageBanner:{
+  width: '100%',
+  height: 500,
+  marginTop: 15,
+  borderRadius: 10
+},
+textBanner:{
+  color: 'white',
+  width: '90%',
+  fontSize: 30,
+  marginTop: 18,
+fontWeight:'bold'
 }
 });
 
 // header//
+
+// BANNER
 
